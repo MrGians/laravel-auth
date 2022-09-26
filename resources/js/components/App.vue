7ce0a4c9c42cb3a5f5688b850d761b560927c892
MrGians <gianlumura97@gmail.com>
2022-09-26 17:22:25 +0200

    set Vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="title m-b-md">Guest Home | WIP</div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -62,6 +62,8 @@
                 margin-bottom: 30px;
             }
         </style>
+        {{-- Vue --}}
+        <script defer src="{{ asset('js/front.js') }}"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -80,20 +82,9 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel | Guest Home
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <div id="root"></div>
+                
             </div>
         </div>
     </body>
