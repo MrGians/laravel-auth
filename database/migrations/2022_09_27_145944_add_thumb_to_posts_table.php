e7043f27450bd22108619273a7176d3b4df23605
MrGians <gianlumura97@gmail.com>
2022-09-27 17:08:54 +0200

    fix position of Thumb column

--- database/migrations/2022_09_27_145944_add_thumb_to_posts_table.php
+++ database/migrations/2022_09_27_145944_add_thumb_to_posts_table.php
@@ -14,7 +14,7 @@ class AddThumbToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('thumb')->nullable();
+            $table->string('thumb')->nullable()->after('content');
         });
     }
 
