2205e4b32845517d6ff982abb246de61c6379fb8
MrGians <gianlumura97@gmail.com>
2022-09-27 17:27:49 +0200

    added index page

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="mb-4 d-flex align-items-center justify-content-between">
+    <h1>Lista Post</h1>
+    <a class="btn btn-sm btn-success ml-2" href="{{ route('admin.posts.create') }}">
+      <i class="fa-solid fa-plus"></i> Crea nuovo
+    </a>
+  </div>
+  <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titolo</th>
+      <th scope="col">Slug</th>
+      <th scope="col">Ultima Modifica</th>
+      <th scope="col">Data Creazione</th>
+      <th scope="col">Azioni</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse ($posts as $post)
+    <tr>
+      <th scope="row">{{ $post->id }}</th>
+      <td>{{ $post->title }}</td>
+      <td>{{ $post->slug }}</td>
+      <td>{{ $post->updated_at }}</td>
+      <td>{{ $post->created_at }}</td>
+      <td class="d-flex">
+        <a class="btn btn-sm btn-primary ml-2" href="{{ route('admin.posts.show', $post) }}">
+          <i class="fa-solid fa-eye"></i>
+        </a>
+        <a class="btn btn-sm btn-warning ml-2" href="{{ route('admin.posts.edit', $post) }}">
+          <i class="fa-solid fa-pen-to-square"></i>
+        </a>
+        <form action="{{ route('admin.posts.destroy', $post) }}" method="POST" class="ml-2">
+          @method('DELETE')
+          @csrf
+          <button class="btn btn-sm btn-danger btn-outline" type="submit">
+            <i class="fa-solid fa-trash-can"></i>
+          </button>
+        </form>
+      </td>
+    </tr>
+    @empty
+    <tr>
+      <th class="text-center h2" colspan="6">Non è presente nessun Post.</th>
+    </tr>
+    @endforelse
+    
+  </tbody>
+</table>
+</div>
+@endsection
