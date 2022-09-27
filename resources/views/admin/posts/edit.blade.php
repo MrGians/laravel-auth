bb2015aaf69b14eecbf660da5564276136bf8dc6
MrGians <gianlumura97@gmail.com>
2022-09-27 18:00:38 +0200

    added edit page

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+      <h1 class="mb-4">Modifica Post</h1>
+      <hr/>
+      @include('includes.admin.posts.form')
+    </div>
+@endsection
