4aeb7cc8b5f240a13851a8402c5a0fd0b3e1698e
MrGians <gianlumura97@gmail.com>
2022-09-28 16:56:05 +0200

    add rand category to Post Seeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,7 +3,9 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -14,9 +16,12 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i = 1; $i <= 10; $i++){
+        $category_id_list = Category::pluck('id')->toArray();
+
+        for($i = 1; $i <= 20; $i++){
             $new_post = new Post();
             $new_post->title = $faker->sentence();
+            $new_post->category_id = Arr::random($category_id_list);
             $new_post->slug = Str::slug($new_post->title, '-');
             $new_post->content = $faker->paragraphs('3', true);
             $new_post->save();
