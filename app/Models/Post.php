fa83b05ad63ade6c9b188fa7644fb92b625e18bc
MrGians <gianlumura97@gmail.com>
2022-09-28 15:45:48 +0200

    fix Post model relationship function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
 
     public function category()
     {
-        return $this->hasOne('App/Models/Category');
+        return $this->belongsTo('App/Models/Category');
     }
 
 
