dbb8ecf9d41c132b9f270efdc60f19cd1cbf0c1b
MrGians <gianlumura97@gmail.com>
2022-09-29 15:12:46 +0200

    added user-post relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'slug', 'content', 'thumb', 'category_id', 'is_published'];
+    protected $fillable = ['title', 'slug', 'content', 'thumb', 'category_id', 'is_published', 'user_id'];
 
     public function category()
     {
         return $this->belongsTo('App\Models\Category');
     }
 
-
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([CategorySeeder::class, UserSeeder::class, PostSeeder::class]);
+        $this->call([
+            UserSeeder::class, 
+            CategorySeeder::class, 
+            PostSeeder::class,
+        ]);
     }
 }
