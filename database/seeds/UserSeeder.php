4cd0d7a05fa708fc53dc5d72f7403853fcc317c9
MrGians <gianlumura97@gmail.com>
2022-09-29 15:13:33 +0200

    added faker users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use Faker\Generator as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -10,12 +11,20 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         $new_user = new User;
-        $new_user->name = 'gianluca';
+        $new_user->name = 'Gianluca';
         $new_user->email = 'gianlumura97@gmail.com';
         $new_user->password = bcrypt('password');
         $new_user->save();
+
+        for($i=1; $i <= 10; $i++){
+            $new_user = new User;
+            $new_user->name = $faker->userName();
+            $new_user->email = $faker->email();
+            $new_user->password = bcrypt('password');
+            $new_user->save();
+        }
     }
 }
