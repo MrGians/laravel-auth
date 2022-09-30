f0eaf1d804d0e936e831762223fb1afefb6f8c59
MrGians <gianlumura97@gmail.com>
2022-09-30 15:16:03 +0200

    fix destroy on PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -156,7 +156,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if(count($post->tags)) $post->tags()->detach();
         $post->delete();
+        
         return redirect()->route('admin.posts.index')
         ->with('message', 'Il Post è stato eliminato correttamente')->with('type', 'success');
     }
